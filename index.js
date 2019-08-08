51e318cf735289e552bb26d5248786752a10dd40
Shunshun Lyu <shunshunlyu@protonmail.com>
2019-08-07 17:10:09 -0700

    added javaScript and more css

---
+++ index.js
@@ -0,0 +1,92 @@
+let questionNumber = 0;
+let score = 0;
+
+function startQuiz() {
+    $(".quizStart").on("click", ".startButton", function(event){
+        $(".quizStart").remove();
+        $(".questionNumber").text(1);
+        renderQuestion();
+        answerCorrectOrNot();
+        nextQuestion();
+    });
+}
+
+function generateQuestion() {
+    if (questionNumber < STORE.length) {
+        return `<div class="question-${questionNumber}">
+        <h2>${STORE[questionNumber].question}</h2>
+        <form>
+        <fieldset>
+        <label class="answerOption">
+        <input type="radio" value="${STORE[questionNumber].answers[0]}" name="answer" required>
+        <span>${STORE[questionNumber].answers[0]}</span>
+        </label>
+        <label class="answerOption">
+        <input type="radio" value="${STORE[questionNumber].answers[1]}" name="answer" required>
+        <span>${STORE[questionNumber].answers[1]}</span>
+        </label>
+        <label class="answerOption">
+        <input type="radio" value="${STORE[questionNumber].answers[2]}" name="answer" required>
+        <span>${STORE[questionNumber].answers[2]}</span>
+        </label>
+        <label class="answerOption">
+        <input type="radio" value="${STORE[questionNumber].answers[3]}" name="answer" required>
+        <span>${STORE[questionNumber].answers[3]}</span>
+        </label>
+        <button type="submit" class="submitButton">Submit</button>
+        </fieldset>
+        </form>
+        </div>`;
+    } else {
+        renderResults();
+        restartQuiz();
+        $('.questionNumber').text(10)
+      }
+}
+
+function renderQuestion () {
+    $('.questionAnswerForm').html(generateQuestion());
+}
+
+function renderResults() {
+    $('.questionAnswerForm').html(`<div class="results correctFeedBack"><h1>Good job finishing the quiz! You've got ${score} out of 10 correct!!</h1><button class="restartButton">Startover!</button></div>`)
+}
+
+function restartQuiz() {
+    $('main').on('click', '.restartButton', function (event) {
+        location.reload();
+      });
+}
+
+function answerCorrectOrNot() {
+    $('form').on('submit', function(event){
+        event.preventDefault();
+        let selected = $('input:checked');
+        let answer = selected.val();
+        let correctAnswer = `${STORE[questionNumber].correctAnswer}`;
+        if (answer === correctAnswer) {
+            selected.parent().addClass('correct');
+            $('.questionAnswerForm').html(`<div class="correctFeedback"><p><b>You got it right! Congratulations!🎆</b></p><button type=button class="nextButton">Next</button></div>`);
+            score++;
+            $('.score').text(score);
+        } else {
+            selected.parent().addClass('wrong');
+            $('.questionAnswerForm').html(`<div class="correctFeedback"><p><b>You got it wrong</b><br>The correct answer is <span>"${correctAnswer}"</span></p><button type=button class="nextButton">Next</button></div>`);
+          }
+    })
+}
+
+function nextQuestion() {
+    $('main').on("click",'.nextButton', function(event){
+        questionNumber++;
+        $('.questionNumber').text(questionNumber+1);
+        renderQuestion();
+        answerCorrectOrNot();
+    })
+}
+
+function createQuiz() {
+    startQuiz();
+}
+
+$(createQuiz);
---
+++ question.js
@@ -0,0 +1,102 @@
+const STORE = [
+    {
+        question: 'Which one is the most balanced workout plan?' ,
+        answers: [
+            'running, weight lifting, yoga',
+            'running, biking, swiming',
+            'biking, weight lifting, swiming',
+            'yoga, pilate, swiming'
+        ],
+        correctAnswer:'running, weight lifting, yoga'
+    },
+    {
+        question: 'Which one is the toughest workout?' ,
+        answers: [
+            'walking',
+            'running',
+            'shopping',
+            'sprinting'
+        ],
+        correctAnswer: 'sprinting'
+    },
+    {
+        question: 'What target heart rate should you aim for during cardio?' ,
+        answers: [
+            'twice your resting heart rate' ,
+            'three times your resting heart rate' ,
+            '50 to 80% of max heart rate' ,
+            'resting hear rate'
+        ],
+        correctAnswer: '50 to 80% of max heart rate'
+    },
+    {
+        question: 'what muscle group does squat workout?' ,
+        answers: [
+            'quads' ,
+            'back' ,
+            'biceps' ,
+            'forearms'
+        ],
+        correctAnswer: 'quads'
+    },
+    {
+        question: 'what muscle group does pull ups workout?' ,
+        answers: [
+            'back' ,
+            'hamstrings' ,
+            'butt',
+            'quads'
+        ],
+        correctAnswer: 'back'
+    },
+    {
+        question: 'what is a superset?' ,
+        answers: [
+            "super man's set" ,
+            'quads workout' ,
+            'pull ups' ,
+            'hiit exercise of strength training'
+        ],
+        correctAnswer: 'hiit exercise of strength training'
+    },
+    {
+        question: 'how many days a week is best for strength training?' ,
+        answers: [
+            '7 days' ,
+            '0 days' ,
+            '5 days in a row' ,
+            '3-4 days a week with days in between for rest'
+        ],
+        correctAnswer: '3-4 days a week with days in between for rest'
+    },
+    {
+        question: 'why do you rest between strength training?' ,
+        answers: [
+            "don't want to work out" ,
+            "ate too much can't workout" ,
+            'need rest to build muscle' ,
+            "don't want to sweat"
+        ],
+        correctAnswer: 'need rest to build muscle'
+    },
+    {
+        question: 'what is bodyweight trainging good for?',
+        answers: [
+            'models' ,
+            'better motivation' ,
+            'eating food after' ,
+            'no access to the gym or equipment'
+        ],
+        correctAnswer: 'no access to the gym or equipment'
+    },
+    {
+        question: 'what not to eat before workout?' ,
+        answers: [
+            'fish' ,
+            'hard-boiled egg' ,
+            'cookie' ,
+            'protein shake'
+        ],
+        correctAnswer: 'cookie'
+    }
+];
